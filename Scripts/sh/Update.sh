020f599b55e065e55412e5a23a6f03742b488ae0
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2021-12-08 10:49:35 +0800

    Update Update.sh

--- Scripts/sh/Update.sh
+++ Scripts/sh/Update.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034,2154,2188
 
 <<'COMMENT'
 cron: 32 6,18 * * *
@@ -26,91 +27,91 @@ TG_USER_ID=''
 TG_PROXY_HOST=''
 TG_PROXY_PORT=''
 openCardBean=''
-Recombin_CK_Mode='3'
-Recombin_CK_ARG1='1'
-Recombin_CK_ARG2='2'
-Remove_Void_CK='1'
+Recombin_CK_Mode=''
+Recombin_CK_ARG1=''
+Recombin_CK_ARG2=''
+Remove_Void_CK=''
+js_deps_replace_envs='js_deps_replace_envs="jdCookie|ql|JD_DailyBonus&sendNotify@JDHelloWorld_jd_scripts|ccwav_QLScript2"'
 
 CollectedRepo='4'
 OtherRepo='3 5 6 9 10'
-Ninja='off'
+RawScript=''
+Ninja='down'
 
 repoNum='4'
-HelpType='HelpType="0"'
+HelpType='HelpType=""'
 BreakHelpType='BreakHelpType="1"'
 BreakHelpNum='BreakHelpNum="31-1000"'
+FixDependType='FixDependType=""'
 package_name='package_name="@types/node axios canvas crypto-js date-fns dotenv download form-data fs global-agent got jieba js-base64 jsdom json5 md5 png-js prettytable qrcode-terminal requests require tough-cookie tslib ts-md5 tunnel typescript ws"'
 front_num='front_num="1"'
 
 update_config() {
-    curl -sfL https://git.io/config.sh -o $file_raw_config
-    mv -b $file_raw_config $dir_config
-    sed -ri "s/GithubProxyUrl=\"https\:\/\/ghproxy.com\/\"/GithubProxyUrl=\"${GithubProxyUrl}\"/g" $file_config_config
-    sed -i "s/TG_BOT_TOKEN=\"\"/TG_BOT_TOKEN=\"${TG_BOT_TOKEN}\"/g" $file_config_config
-    sed -i "s/TG_USER_ID=\"\"/TG_USER_ID=\"${TG_USER_ID}\"/g" $file_config_config
-    sed -i "s/openCardBean=\"30\"/openCardBean=\"${openCardBean}\"/g" $file_config_config
-    sed -i "s/Recombin_CK_Mode=\"\"/Recombin_CK_Mode=\"${Recombin_CK_Mode}\"/g" "$file_config_config"
-    sed -i "s/Recombin_CK_ARG1=\"\"/Recombin_CK_ARG1=\"${Recombin_CK_ARG1}\"/g" "$file_config_config"
-    sed -i "s/Recombin_CK_ARG2=\"\"/Recombin_CK_ARG2=\"${Recombin_CK_ARG2}\"/g" "$file_config_config"
-    sed -i "s/Remove_Void_CK=\"\"/Remove_Void_CK=\"${Remove_Void_CK}\"/g" "$file_config_config"
+    curl -sL https://cdn.jsdelivr.net/gh/Oreomeow/VIP@main/Conf/Qinglong/config.sample.sh >"$file_raw_config"
+    mv -b "$file_raw_config" "$dir_config"
+    sed -ri "s/GithubProxyUrl=\"https\:\/\/ghproxy.com\/\"/GithubProxyUrl=\"${GithubProxyUrl}\"/" "$file_config_config"
+    sed -i "s/TG_BOT_TOKEN=\"\"/TG_BOT_TOKEN=\"${TG_BOT_TOKEN}\"/" "$file_config_config"
+    sed -i "s/TG_USER_ID=\"\"/TG_USER_ID=\"${TG_USER_ID}\"/" "$file_config_config"
+    sed -i "s/TG_PROXY_HOST=\"\"/TG_PROXY_HOST=\"${TG_PROXY_HOST}\"/" "$file_config_config"
+    sed -i "s/TG_PROXY_PORT=\"\"/TG_PROXY_PORT=\"${TG_PROXY_PORT}\"/" "$file_config_config"
+    sed -i "s/openCardBean=\"30\"/openCardBean=\"${openCardBean}\"/" "$file_config_config"
+    sed -i "s/Recombin_CK_Mode=\"\"/Recombin_CK_Mode=\"${Recombin_CK_Mode}\"/" "$file_config_config"
+    sed -i "s/Recombin_CK_ARG1=\"\"/Recombin_CK_ARG1=\"${Recombin_CK_ARG1}\"/" "$file_config_config"
+    sed -i "s/Recombin_CK_ARG2=\"\"/Recombin_CK_ARG2=\"${Recombin_CK_ARG2}\"/" "$file_config_config"
+    sed -i "s/Remove_Void_CK=\"\"/Remove_Void_CK=\"${Remove_Void_CK}\"/" "$file_config_config"
+    sed -i "/^js_deps_replace_envs=/c${js_deps_replace_envs}" "$file_config_config"
 }
 
 update_extra() {
-    curl -sfL https://git.io/extra.sh -o $file_raw_extra
-    mv -b $file_raw_extra $dir_config
-    sed -i "s/CollectedRepo=(4)/CollectedRepo=(${CollectedRepo})/g" $file_config_extra
-    sed -i "s/OtherRepo=()/OtherRepo=(${OtherRepo})/g" $file_config_extra
-    sed -i "s/Ninja=\"on\"/Ninja=\"${Ninja}\"/" $file_config_extra
-    sed -i '/NOWTIME=/a\\tcp \/ql\/config\/sendNotify.js \/ql\/scripts\/sendNotify.js' $file_config_extra
-    # echo 'cat /ql/db/wskey.list >> /ql/config/wskey.list && :> /ql/db/wskey.list' >> $file_config_extra
+    curl -sL https://cdn.jsdelivr.net/gh/Oreomeow/VIP@main/Tasks/qlrepo/extra.sh >"$file_raw_extra"
+    mv -b "$file_raw_extra" "$dir_config"
+    sed -i "s/CollectedRepo=(4)/CollectedRepo=(${CollectedRepo})/" "$file_config_extra"
+    sed -i "s/OtherRepo=()/OtherRepo=(${OtherRepo})/" "$file_config_extra"
+    sed -i "s/RawScript=(1 2)/RawScript=(${RawScript})/" "$file_config_extra"
+    sed -i "s/Ninja=\"on\"/Ninja=\"${Ninja}\"/" "$file_config_extra"
+    echo 'bash /ql/config/Update.sh' >>"$file_config_extra"
 }
 
 update_code() {
-    curl -sfL https://git.io/code.sh -o $file_raw_code
-    mv -b $file_raw_code $dir_config
-    # sed -i "s/repo=\$repo4/repo=\$repo${repoNum}/g" $file_config_code
-    sed -i "/^HelpType=/c${HelpType}" $file_config_code
-    sed -i "/^BreakHelpType=/c${BreakHelpType}" $file_config_code
-    sed -i "/^BreakHelpNum=/c${BreakHelpNum}" $file_config_code
+    curl -sL https://cdn.jsdelivr.net/gh/Oreomeow/VIP@main/Scripts/sh/Helpcode2.8/code.sh >"$file_raw_code"
+    mv -b "$file_raw_code" "$dir_config"
+    sed -i "s/repo=\$repo4/repo=\$repo${repoNum}/" "$file_config_code"
+    sed -i "/^HelpType=/c${HelpType}" "$file_config_code"
+    sed -i "/^BreakHelpType=/c${BreakHelpType}" "$file_config_code"
+    sed -i "/^BreakHelpNum=/c${BreakHelpNum}" "$file_config_code"
     sed -i "/^package_name=/c${package_name}" "$file_config_code"
+    sed -i "/^FixDependType=/c${FixDependType}" "$file_config_code"
     sed -i "/^front_num=/c${front_num}" "$file_config_code"
 }
 
 update_task_before() {
-    curl -sfL https://git.io/task_before.sh -o $file_raw_task_before
-    mv -b $file_raw_task_before $dir_config
+    curl -sL https://cdn.jsdelivr.net/gh/Oreomeow/VIP@main/Scripts/sh/Helpcode2.8/task_before.sh >"$file_raw_task_before"
+    mv -b "$file_raw_task_before" "$dir_config"
+    sed -i "s/jd_moneyTree_heip/jd_moneyTree*/" "$file_config_task_before"
 }
 
-random_cookie() {
-    c=1000000
-    for r in {1..3}; do
-        p=`expr $c - $r`
-        sed -ri "s/\"position\"\:${p}/regular${r}/" $file_db_env
-    done
-    for line in {1..100}; do
-        sed -ri "${line}s/(\"position\"\:)[^,]*/\"position\"\:${RANDOM}/" $file_db_env
-    done
-    for r in {1..3}; do
-        p=`expr $c - $r`
-        sed -ri "s/regular${r}/\"position\"\:${p}/" $file_db_env
-    done
-    ql check
+update_ninja() {
+    cd /ql && pm2 delete ninja && rm -rf /ql/ninja
+    git clone -b master https://github.com/China-Uncle/Waikiki_ninja ninja
+    cd /ql/ninja/backend || exit
+    pnpm install
+    cp -rf /ql/config/.env .env
+    pm2 start
+    cp -rf /ql/config/sendNotify.js /ql/ninja/backend/sendNotify.js
+		
+					   
+						
+																
+		
+			
 }
 
 update_config
 update_extra
 update_code
 update_task_before
-
-if [ $(date "+%H") -eq 18 ]; then
-    random_cookie
-fi
-
-case $@ in
-    ck)
-      random_cookie
-      ;;
-esac
+# update_ninja
+								 
 echo "ql check" >> $file_config_extra
 
 echo "
