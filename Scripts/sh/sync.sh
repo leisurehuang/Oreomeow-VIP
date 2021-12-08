5686283d76acf0e1c8914905595829e3d7bf40b9
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2021-12-08 20:41:25 +0800

    Update sync.sh

--- Scripts/sh/sync.sh
+++ Scripts/sh/sync.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2034,2154,2188
 
 <<'COMMENT'
 cron: 32 6,18 * * *
@@ -12,15 +11,6 @@ dir_shell=/ql/shell
 
 file_config_config=$dir_config/config.sh
 file_config_update=$dir_config/Update.sh
-
-echo "ql check" >> $file_config_extra
-echo "
-if [[ $(date "+%-H") -eq 0 || $(date "+%-H") -eq 8 || $(date "+%-H") -eq 16 ]] && [ $(date "+%-M") -eq 0 ] && [ $(date "+%-S") -gt 4 ]; then
-  export JD_JOY_REWARD_NAME="20"
-else
-  export JD_JOY_REWARD_NAME="500"
-fi
-" >>$file_config_config
 
 update_update() {
     curl -sL https://cdn.jsdelivr.net/gh/Oreomeow/VIP@main/Scripts/sh/Update.sh >"$file_config_update"
