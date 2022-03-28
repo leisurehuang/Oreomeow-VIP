998d8f1215a94622cedb8ccc3ebf29ca48ae3800
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-03-28 23:33:17 +0800

    Update 1custom.sh

--- Scripts/sh/1custom.sh
+++ Scripts/sh/1custom.sh
@@ -26,7 +26,7 @@ add_sync() {
         echo "添加 sync.sh"
         # 获取token
         token=$(cat $dir_config/auth.json | jq --raw-output .token)
-        curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"更新配置和任务","command":"task /ql/config/sync.sh","schedule":"16 6 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1624782068473'
+        curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"更新配置和任务","command":"task /ql/data/config/sync.sh","schedule":"16 6 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1624782068473'
     fi
 }
 # 运行一次 Update
